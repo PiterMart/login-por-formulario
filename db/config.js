7c0966afc01e24272fbfc6bcc01e8d8554d31175
PiterMart <pitermartingaste@gmail.com>
2022-06-08 20:07:35 -0300

    Uploading for correction

---
+++ db/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    DB_CONFIG: {
+        firebase: {
+            credential: {
+                "type": "service_account",
+                "project_id": "ecommerce-7100b",
+                "private_key_id": "ad13f1e7a71a64e832aab56fcde7183a1a20918b",
+                "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCwN6J/+40aadaP\n6sWQWq0+mJpCwfgB28FzTk5YKMwmoMPc1w6mKvzf6Vb5qRhfq8mhNVJCTh6PfZMd\n5/KVWVvr+QrX3GvIlnuweS+LquN3Zn3AvaF9NOTikE8zf+Pq5vOUXZ1yJuePl8DK\nqLnGa9v6vxUDIFvznziikPymkqOc4T2FoT+lXT3mAZHc+48LpMsXtp6yaR1wxrPf\n9p3HcmIfEx2HoEx4+3huZ/SdLFFNNrv9NhcFtPz5tBDz3H1Bu0vdA5DxM8gkQKCp\nwBarvsPc98zgn8OJclqvt2Z61qenmOBhIp3ZduKsJDj3jWpH1XPpsY+Fp5qgYIP9\nvx6b7YE9AgMBAAECggEAE57OMBPRHSzFoBaS0sTV309cdaxlMcfHHVCNtpEsUey6\ngh9L2UKWD1RKw9eJCDX3aESomQtbqbxgogbOYsSlUHvVMj2vnpDqyrZjjdiEJSo7\nPDehAHDgPz20I9bMiL4YRt+5hRlB1SIfbDEnJimiP6QA8e0rb8+9nZQpHV7car4V\nzJLTZzJzudkQSMZOeuJ/jYUPmVHCDZrsOIzO04SfShcGhLPUCdr9xJyCs4L6mf3y\n70lp8uBbaJMbdY7hRPmhTZjhvhvoQLup3CgmGVKD70iY62idnc55zoIjTp8Cmzwh\nLkg6iBrgB49BkwFZ9mQKmnGXSqRkp9WgnD4lsd0anQKBgQDxeCXws+5hgeqAUV6Y\nugIja3koraSHHbKsvvgQ592jrxxm12iPt4YFHdgGM0E6njUthPezl6VTub/rKOYn\nL3FGya/nmHrExv6UBIrsXDontI0VmP85AcswTvLSTFjjsuww+++QoiD+TXuXtsCz\n9SnEMEX/EWKDi/4qO+DeMzbdWwKBgQC60kZee+kOsHkV5rzg8lnXs2AeZzuuPQMN\nxy5a52OHgfNBEJ0H4Ro/gtyBnvCXuDass/FNkCaqX/dfwvOSGneMNPb+4Qg3uHn8\nX4hfetLn8as57affYXHIbVRa5lCJHPWAr32b/ez0UTZBQdHV3xt+8Sj8d15E7Mz8\nr9gURjvnRwKBgQC82fMSZicyK6Fz6i/J0vCJMyz3G5dp9VggstrgnfdCZVoZNK+a\ndzZiGg0yDDQf3ExK5Yu08g1jhgsLqFkYnIpDisoAn8ikzd2ddlAQu7DgDN2q3pX9\nx+FaTuSqhPugWfdP+DpNLAAR5BedeFkOZdfckMSt0LLznzGRZnNgVT27sQKBgHYJ\n5exM3jQpzFfKMgzvE1T2QQT0tQCiDAvojaFiEcDMmqXBmXYhBDScujYr9lfmTKVS\nDGS1b7i2adZXlw35Kw+rZ2QjSs3qASk0MzYgpKFoUIeJYUgCiaHZcjL80mXPpk5o\nkzS8e7rilSUmMS6wiO7r8LmTsjxaYipF8IDe33qBAoGATPriC0wfbBIw0VfPpR2B\nDJIuGf1jcpjF4MDfi1qzHgfjHybJ0KsZSaYQizPP6yBKHxRz4bfD3Wu1Jos6tAIU\nowTECyYy9z9/THxCkoiTF5KRpTsoiZtK0loxe+DFebSyluBSFGzhs+KARxNWw5TK\njocgmSqEZ6tBcIB3oOPxWaU=\n-----END PRIVATE KEY-----\n",
+                "client_email": "firebase-adminsdk-5jgx5@ecommerce-7100b.iam.gserviceaccount.com",
+                "client_id": "115778961603346471861",
+                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+                "token_uri": "https://oauth2.googleapis.com/token",
+                "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+                "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-5jgx5%40ecommerce-7100b.iam.gserviceaccount.com"
+            }
+
+        }
+    }
+};
